b5b8c15ee4bbf041692b97b3dbdeb71be938530f
dhrubaghosh3127-art <dhrubaghosh3127@gmail.com>
2026-03-14 17:26:48 +0600

    Create firebase.ts

---
+++ src/firebase.ts
@@ -0,0 +1,16 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth, GoogleAuthProvider } from 'firebase/auth';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyDDZc9JVd-yKys2BR55IHTdEjAkh-cEbXs',
+  authDomain: 'ask-gpt-2f61d.firebaseapp.com',
+  projectId: 'ask-gpt-2f61d',
+  storageBucket: 'ask-gpt-2f61d.firebasestorage.app',
+  messagingSenderId: '280092322654',
+  appId: '1:280092322654:web:5e9599877929634466bdcf',
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const googleProvider = new GoogleAuthProvider();
