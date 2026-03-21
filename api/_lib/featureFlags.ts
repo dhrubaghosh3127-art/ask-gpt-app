fd90309657d0049e4d685cd1c9d4d306eba99dcb
dhrubaghosh3127-art <dhrubaghosh3127@gmail.com>
2026-03-21 20:33:22 +0600

    Create featureFlags.ts

---
+++ api/_lib/featureFlags.ts
@@ -0,0 +1,8 @@
+export const CONTROLLER_V2_CONFIG = {
+  plannerMinConfidence: 0.45,
+  maxFeedbackRetries: 2,
+  enableFeedbackLoop: true,
+  enableFastPath: true,
+  fastPathMaxPromptLength: 120,
+  useImagePrecheck: true,
+} as const;
