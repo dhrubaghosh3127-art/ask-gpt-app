bbb589bf4cbdb3466cbaead220fb4c53e66b84e1
dhrubaghosh3127-art <dhrubaghosh3127@gmail.com>
2026-04-26 16:00:38 +0600

    Update firebase.ts

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,5 +1,6 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth, GoogleAuthProvider } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
   apiKey: 'AIzaSyDDZc9JVd-yKys2BR55IHTdEjAkh-cEbXs',
@@ -12,5 +13,6 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 
+export const db = getFirestore(app);
 export const auth = getAuth(app);
 export const googleProvider = new GoogleAuthProvider();
