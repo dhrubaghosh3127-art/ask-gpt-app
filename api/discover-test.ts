bb2f89cded3de9634fd74fb46132760f278d1b2b
dhrubaghosh3127-art <dhrubaghosh3127@gmail.com>
2026-05-03 00:50:37 +0600

    Update discover-test.ts

--- api/discover-test.ts
+++ api/discover-test.ts
@@ -1,4 +1,4 @@
-import { db } from "./_lib/firebaseAdmin";
+import { db } from "./_lib/firebaseAdmin.js";
 
 export default async function handler(req: any, res: any) {
   if (req.method !== "GET") {
